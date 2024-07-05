bd3deca2e2c2959e99e38db7bd0e3cc5d477b97d
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2024-07-05 16:30:11 +0100

    style: add custom styling to styles and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        custom: "0px 8px 3px 0.5px rgba(23, 23, 23, 0.3)",
+        custom_hover: "0px 16px 3px 0.5px rgba(23, 23, 23, 0.3)",
+      },
       colors: {
-        primary: '#F4F4F4',
-        secondary: '#171717',
+        primary: "#F4F4F4",
+        secondary: "#171717",
       },
       fontFamily: {
-        'montserrat': ['"Montserrat"', 'sans-serif'],
+        montserrat: ['"Montserrat"', "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
